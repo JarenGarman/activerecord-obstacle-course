9b35447db912ed05f8efa6576ae158b2e32b9d6d
Jaren Garman <jarengarman@protonmail.com>
2025-03-12 12:20:45 -0400

    feat: Complete test 13

--- spec/aroc/aroc_week_4_spec.rb
+++ spec/aroc/aroc_week_4_spec.rb
@@ -110,13 +110,12 @@ describe 'ActiveRecord Obstacle Course, Week 4' do
     expected_result = [@item_4, @item_2, @item_5, @item_3]
 
     # ----------------------- Using Ruby -------------------------
-    order = Order.find(@order_3.id)
-    grouped_items = order.items.sort_by { |item| item.name }
+    # order = Order.find(@order_3.id)
+    # grouped_items = order.items.sort_by { |item| item.name }
     # ------------------------------------------------------------
 
     # ------------------ Using ActiveRecord ----------------------
-    # HINT: This query does not need grouping or aggregating
-    # Solution goes here
+    grouped_items = Order.find(@order_3.id).items.order(:name)
     # ------------------------------------------------------------
 
     # Expectation
