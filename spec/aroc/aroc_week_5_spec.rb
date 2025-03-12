c74ecf9aebfbe85d224fcbea1adbdfde14e499b2
Jaren Garman <jarengarman@protonmail.com>
2025-03-12 15:15:31 -0400

    feat: Complete test 26

--- spec/aroc/aroc_week_5_spec.rb
+++ spec/aroc/aroc_week_5_spec.rb
@@ -245,20 +245,18 @@ describe 'ActiveRecord Obstacle Course, Week 5' do
     expected_result = ['Abercrombie', 'Banana Republic', 'Calvin Klein', 'Dickies', 'Eddie Bauer', 'Fox', 'Giorgio Armani', 'Izod', 'J.crew']
 
     # ----------------------- Using Ruby -------------------------
-    items = Item.all
+    # items = Item.all
 
-    ordered_items = items.map do |item|
-      item if item.orders.present?
-    end.compact
+    # ordered_items = items.map do |item|
+    #   item if item.orders.present?
+    # end.compact
 
-    ordered_items_names = ordered_items.map(&:name)
-    ordered_items_names.sort
+    # ordered_items_names = ordered_items.map(&:name)
+    # ordered_items_names.sort
     # ------------------------------------------------------------
 
     # ------------------ Using ActiveRecord ----------------------
-    # Solution goes here
-    # When you find a solution, experiment with adjusting your method chaining.
-    # Which ones are you able to switch around without relying on Ruby's Enumerable methods?
+    ordered_items_names = Item.joins(:order_items).order(:created_at).pluck(:name).uniq
     # ---------------------------------------------------------------
 
     # Expectations
